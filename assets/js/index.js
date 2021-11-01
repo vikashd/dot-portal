4d93296fa03cddd429138987c471ca04bd7b29ca
Vikash Deepchand <vikash.deepchand@gmail.com>
2021-11-01 19:26:21 +1300

    Feedback updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,6 +35,68 @@
 })();
 
 /**
+ * Account dropdown menu
+ */
+(function () {
+    var accountMenuBtn = document.getElementById("account-menu-btn");
+
+    if (!accountMenuBtn) {
+        return;
+    }
+
+    var offset = {
+        name: "offset",
+        options: {
+            offset: ({ placement, reference, popper }) => {
+                return [-18, 20];
+            },
+        },
+    };
+
+    new bootstrap.Dropdown(accountMenuBtn, {
+        popperConfig: function (defaultBsPopperConfig) {
+            return Object.assign(defaultBsPopperConfig, { modifiers: [offset] });
+        },
+    });
+})();
+
+/**
+ * Notification bar
+ */
+(function () {
+    var bar = document.getElementById("notification-bar");
+
+    if (!bar) {
+        return;
+    }
+
+    var closeBtn = document.getElementById("notification-close");
+
+    function resize() {
+        document.body.style.paddingTop = bar.offsetHeight + "px";
+    }
+
+    function close() {
+        document.body.style.paddingTop = "";
+        bar.style.transform = "translateY(-100%)";
+
+        bar.addEventListener("transitionend", function () {
+            document.body.classList.remove("body-notification");
+            window.removeEventListener("resize", resize);
+            bar.style.visibility = "hidden";
+        });
+    }
+
+    document.body.classList.add("body-notification");
+    document.body.style.paddingTop = bar.offsetHeight + "px";
+    bar.style.visibility = "visible";
+    bar.style.transform = "translateY(0)";
+
+    window.addEventListener("resize", resize);
+    closeBtn.addEventListener("click", close);
+})();
+
+/**
  * Mobile menu
  */
 (function () {
