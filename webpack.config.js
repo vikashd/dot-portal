6360c28c3b5b23524512008c9d06202a29075db7
Vikash Deepchand <vikash.deepchand@gmail.com>
2022-02-26 22:19:27 +1300

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    devtool: 'source-map',
+    devtool: isProduction ? 'source-map' : null,
     entry: {
         index: path.join(__dirname, 'src', 'js', 'index'),
     },
